fb8aa214e45487da5a2a198fbdc52403c38dc20a
Pavel Nemirovsky <pavelnemirovsky@icloud.com>
2016-12-03 21:42:54 +0200

    Minor fixes after first testing

--- zbx_redis_discovery.sh
+++ zbx_redis_discovery.sh
@@ -1,34 +1,66 @@
 #!/bin/bash
 IFS=$'\n'
 PASSWORDS=( "$@" )
-LIST=$(ps -eo uname,args | grep -v grep | grep redis | tr -s [:blank:] ":")
+LIST=$(ps -eo uname,args | grep -v grep | grep redis-server | tr -s [:blank:] ":")
 
 echo -n '{"data":['
 
+discover_redis_instance() {
+    HOST=$1
+    PORT=$2
+    PASSWORD=$3
+
+    if [[ $PASSWORD != "" ]]; then
+        ALIVE=$(redis-cli -h $HOST -p $PORT -a $PASSWORD ping)
+    else
+        ALIVE=$(redis-cli -h $HOST -p $PORT ping)
+    fi
+
+    if [[ $ALIVE != "PONG" ]]; then
+        return 0
+    elif [[ $PASSWORD != "" ]]; then
+        INSTANCE=$(redis-cli -h $HOST -p $PORT -a $PASSWORD info | grep config_file | rev | cut -d "/" -f1 | rev | tr -d [:space:] | tr -d ".conf" | tr [:lower:] [:upper:])
+    else
+        INSTANCE=$(redis-cli -h $HOST -p $PORT info | grep config_file | rev | cut -d "/" -f1 | rev | tr -d [:space:] | tr -d ".conf" | tr [:lower:] [:upper:])
+    fi
+
+    echo $INSTANCE
+}
+
+generate_json() {
+    HOST=$1
+    PORT=$2
+    INSTANCE=$3
+
+    echo -n '{'
+    echo -n '"#PORT":"'$PORT'",'
+    echo -n '"#HOST":"'$HOST'",'
+    echo -n '"#INSTANCE":"'$INSTANCE'"'
+    echo -n '},'
+}
+
 for s in $LIST; do
-    IP=$(echo $s | cut -d":" -f3)
+    HOST=$(echo $s | cut -d":" -f3)
     PORT=$(echo $s | cut -d":" -f4)
 
     # TRY PASSWORD PER EACH DISCOVERED INSTANCE
     if [[ ${#PASSWORDS[@]} -ne 0 ]]; then
         for (( i=0; i<${#PASSWORDS[@]}; i++ ));
         do
-            INSTANCE=$(redis-cli -h $IP -p $PORT -a ${PASSWORDS[$i]} info | grep config_file | rev | cut -d "/" -f1 | rev | tr -d [:space:] | tr -d ".conf" | tr [:lower:] [:upper:])
-            if [ $? -eq 0 ]; then
-                stdbuf -oL redis-cli -h $IP -p $PORT -a ${PASSWORDS[$i]} info all &> /tmp/redis-$IP-$PORT
+            INSTANCE=$(discover_redis_instance $HOST $PORT ${PASSWORDS[$i]})
+            if [[ -n $INSTANCE ]]; then
+                stdbuf -oL redis-cli -h $HOST -p $PORT -a ${PASSWORDS[$i]} info all &> /tmp/redis-$HOST-$PORT
+                generate_json $HOST $PORT $INSTANCE
                 break
             fi
         done
     else
-        INSTANCE=$(redis-cli -h $IP -p $PORT  info | grep config_file | rev | cut -d "/" -f1 | rev | tr -d [:space:] | tr -d ".conf" | tr [:lower:] [:upper:])
-        stdbuf -oL redis-cli -h $IP -p $PORT  info all &> /tmp/redis-$IP-$PORT
+        INSTANCE=$(discover_redis_instance $HOST $PORT)
+        if [[ -n $INSTANCE ]]; then
+            stdbuf -oL redis-cli -h $HOST -p $PORT  info all &> /tmp/redis-$HOST-$PORT
+            generate_json $HOST $PORT $INSTANCE
+        fi
     fi
-
-    echo -n '{'
-    echo -n '"#PORT":"'$PORT'",'
-    echo -n '"#HOST":"'$IP'",'
-    echo -n '"#INSTANCE":"'$INSTANCE'"'
-    echo -n '},'
 
 done | sed -e 's:\},$:\}:'
 echo -n ']}'
